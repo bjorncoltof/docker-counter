c16bb91194dcc516a9772495c7e89f46bb911a11
Bjorn Coltof <bjorn.coltof@gmail.com>
2019-12-04 15:09:38 +0100

    Add counter console program and grpc service to compare memory usage

---
+++ Grpc-Counter/Services/CounterService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using GrpcCounter.Protos;
+using System.Threading.Tasks;
+using static GrpcCounter.Protos.CounterService;
+
+namespace GrpcCounter.Services
+{
+    public class CounterService : CounterServiceBase
+    {
+        public override Task<GetCounterResponse> GetCounterValue(GetCounterRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(
+                new GetCounterResponse
+                {
+                    Value = 0
+                });
+        }
+    }
+}
